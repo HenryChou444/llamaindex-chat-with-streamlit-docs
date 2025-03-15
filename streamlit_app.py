c4b718a98168a3b733329ce8e019a5fc18f6fc67
HenryChou444 <161707780+HenryChou444@users.noreply.github.com>
2025-03-15 09:58:12 +0100

    tests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
+import glob
+
 import streamlit as st
 import openai
 from llama_index.llms.openai import OpenAI
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.core.readers.json import JSONReader
 
 st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
@@ -18,8 +21,6 @@ if "messages" not in st.session_state.keys():  # Initialize the chat messages hi
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    reader = SimpleDirectoryReader(input_dir="./data/test", recursive=True)
-    docs = reader.load_data()
     Settings.llm = OpenAI(
         model="gpt-3.5-turbo",
         temperature=0.2,
@@ -29,10 +30,26 @@ def load_data():
         to Hey telecom. Keep 
         your answers technical and based on 
         facts – do not hallucinate features.
-        Answer in french""",
+        Answer in french""",)
+    reader = JSONReader(
+        levels_back=0,  # Set levels back as needed
+        collapse_length=None,  # Set collapse length as needed
+        ensure_ascii=False,  # ASCII encoding option
+        is_jsonl=False,  # Set if input is JSON Lines format
+        clean_json=True  # Clean up formatting-only lines
     )
-    index = VectorStoreIndex.from_documents(docs)
+    # Find all JSON files in the specified directory
+    json_files = glob.glob(f"./data/testjson/*.json")
+    # Load the data from each JSON file
+    documents = []
+    for json_file in json_files:
+        documents.extend(reader.load_data(input_file=json_file, extra_info={}))
+
+    # Create an index for querying
+    index = VectorStoreIndex.from_documents(documents)
     return index
+
+
 
 
 index = load_data()
