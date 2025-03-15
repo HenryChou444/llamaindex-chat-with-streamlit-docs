a3c8678c230f4485274682cf9b1150087e30fd2a
HenryChou444 <161707780+HenryChou444@users.noreply.github.com>
2025-03-15 10:46:23 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,24 @@ from llama_index.llms.openai import OpenAI
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.core.readers.json import JSONReader
 
-st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered",
+                   initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
 st.title("Hey Chatbot 💬")
-#st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+# st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+
+# CSS pour changer le fond en bleu
+st.markdown(
+    """
+    <style>
+        /* Changer le fond de la page en bleu */
+        [data-testid="stAppViewContainer"] {
+            background-color: #ADD8E6; /* Bleu clair */
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
@@ -19,18 +33,15 @@ if "messages" not in st.session_state.keys():  # Initialize the chat messages hi
         }
     ]
 
+
 @st.cache_resource(show_spinner=False)
 def load_data():
     Settings.llm = OpenAI(
         model="gpt-3.5-turbo",
         temperature=0.2,
-        system_prompt="""You are an expert of Hey Telecom and your 
-        job is to answer technical questions. 
-        Assume that all questions are related 
-        to Hey telecom. Keep 
-        your answers technical and based on 
         facts – do not hallucinate features.
         Answer in french""",)
+        Answer based on the language of the following message""", )
     reader = JSONReader(
         levels_back=0,  # Set levels back as needed
         collapse_length=None,  # Set collapse length as needed
@@ -50,8 +61,6 @@ def load_data():
     return index
 
 
-
-
 index = load_data()
 
 if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
@@ -60,7 +69,7 @@ if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
     )
 
 if prompt := st.chat_input(
-    "Ask a question"
+        "Ask a question"
 ):  # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
